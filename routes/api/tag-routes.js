39a78c63e5dc6da241fa0d5ac8356cccc3e234ba
Enrique Collazo <ecollazo1991@gmail.com>
2021-03-20 16:03:58 -0400

    updated routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-    //  include: [{ model: Product, ProductTag, as: 'tags_products'}]
+     // include: [{ model: Product} , {model: ProductTag, as: 'tags_products'}]
     })
     res.status(200).json(tagData);
   } catch (err) {
